40e047c621ec5c61ff84d6e3ba681d55b40ec613
Evgeniy <ekjek@mail.ru>
2022-08-29 01:23:31 +0300

    brought beauty

--- js/gallery.js
+++ js/gallery.js
@@ -5,15 +5,13 @@ let newBigImg = document.querySelector('#wrapper')
 function gallery() {
     let nodeStr = ""
     for(let i=1; i<=8; i++) {
-        nodeStr += `<img src="img/${i}.jpg" alt="galimg" id="small_img" class="small-img" onclick="somefunc(${i})">`
+        nodeStr += `<img src="img/${i}.jpg" alt="galimg" id="small_img" class="small-img" width="180px" height="130px" onclick="somefunc(${i})">`
     }
-    carousel.innerHTML = nodeStr
-    
+    carousel.innerHTML = nodeStr    
 }
 
 function somefunc(num) {
-    newBigImg.innerHTML = `<img src="img/${num}.jpg" alt="galimg" id="small_img" class="big-img"  width="500px" height="400px" >`
-      
+    newBigImg.innerHTML = `<img src="img/${num}.jpg" alt="galimg" id="main" class="big-img"  width="500px" height="400px" >`      
 } 
 
 gallery()

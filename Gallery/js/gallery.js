d31e12c98fbf57ce4b819c0d9ff811b6fa93af14
Evgeniy <ekjek@mail.ru>
2022-09-08 00:34:39 +0300

    WIN!!

--- Gallery/js/gallery.js
+++ Gallery/js/gallery.js
@@ -18,9 +18,9 @@ function somefunc(num) {
     newBigImg.innerHTML = `<img src="img/${num}.jpg" alt="logo" id="main" data-num="${num}" class="big-img"  width="500px" height="400px" >`      
 } 
 
-function clickButtonLeft () {
+function clickButton () {
     leftBtn.addEventListener('click', function() {
-        let whatImg = document.getElementById('main').dataset.num
+        let whatImg = +document.getElementById('main').dataset.num
         if (whatImg > 1) {
             newBigImg.innerHTML = `<img src="img/${whatImg - 1}.jpg" alt="logo" id="main" data-num="${whatImg - 1}" class="big-img"  width="500px" height="400px" >`
         }
@@ -28,24 +28,23 @@ function clickButtonLeft () {
             newBigImg.innerHTML = `<img src="img/8.jpg" alt="logo" id="main" data-num="8" class="big-img"  width="500px" height="400px" >`
         }
     })
-}
 
-function clickButtonRight () {
+
     righttBtn.addEventListener('click', function() {
-        let rImg = document.getElementById('main').dataset.num
-        if (rImg < 8) {
-            newBigImg.innerHTML = `<img src="img/${rImg + 1}.jpg" alt="logo" id="main" data-num="${rImg + 1}" class="big-img"  width="500px" height="400px" >`
+        let whatImg = +document.getElementById('main').dataset.num
+        if (whatImg < 8) {
+            newBigImg.innerHTML = `<img src="img/${whatImg + 1}.jpg" alt="logo" id="main" data-num="${whatImg + 1}" class="big-img"  width="500px" height="400px" >`
         }  
         else {
             newBigImg.innerHTML = `<img src="img/1.jpg" alt="logo" id="main" data-num="1" class="big-img"  width="500px" height="400px" >`
 
-        }     
+        }      
     })
 }
 
 
 gallery()
-clickButtonLeft()
-clickButtonRight()
+clickButton()
+
 
 
